f66bef6310cb84143078ba758d73f335bb6406d0
Dean <30427930+nicedaddy@users.noreply.github.com>
2026-09-20 18:01:30 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-export const CONFIG = {
-  // GitHub Releases 직링크는 CORS 문제가 없어 가장 빠르고 안정적입니다.
+\export const CONFIG = {
+  // 생성하신 GitHub Release v1.0.0 직링크
   modelUrl: "https://github.com/nicedaddy/design_review/releases/download/v1.0.0/AMR-S250A_R3_MG.PLY",
 
   getModelUrl() {
